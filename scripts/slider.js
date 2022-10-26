1824ccaffced0796ca02be4e1066dcad6fd1e85e
makino-ryota <ryo.pon.san@gmail.com>
2022-10-27 02:10:00 +0900

    add special page and add function of slider

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,11 +1,12 @@
 // slider用の処理 GypsyR
 class slideImg{
-    constructor(alt, explanation, imgUrl, place, colorCode){
+    constructor(alt, explanation, imgUrl, place, colorCode, nextUrl){
         this.alt = alt;
         this.explanation = explanation;
         this.imgUrl = imgUrl;
         this.place = place;
         this.colorCode = colorCode;
+        this.nextUrl = nextUrl;
     }
     // 画像位置設定
     placeTable = {
@@ -25,10 +26,10 @@ class slideImg{
         // target
         let domObj = document.getElementById("slider");
         domObj.classList.add("box");
-        // slider div
+        // slider用 div
         let divSlider = document.createElement("div");
         divSlider.classList.add("slider__content");
-        
+        // sliderに入れる一要素のdiv
         let divSub = document.createElement("div");
         divSub.classList.add("divSubContainer");
 
@@ -45,8 +46,6 @@ class slideImg{
         // DOM
         divSub.append(imgSlider);
         divSub.append(pSlider);
-
-        console.log(divSub);
         // アニメーションをもつスライダーにappend
         divSlider.append(divSub);
         domObj.append(divSlider);
@@ -61,23 +60,25 @@ class helperFunction{
         }
     }
 }
-//各要素作成
-let sImg1 = new slideImg("slider_image1", "【特集】this is coffee", "images/coffee-2151200_1280.jpg","left", "c1");
-let sImg2 = new slideImg("slider_image2", "【特集】Wow pancakes, awesome", "images/pancakes-5989136_1280.jpg","center", "c3");
-let sImg3 = new slideImg("slider_image3", "【新メニュー!】Wow cake", "images/cake-1284548_1280.jpg","right", "c1");
-let sImg4 = new slideImg("slider_image4", "Apple pie yummy", "images/apple-pie-5479993_1280.jpg","left", "c3");
-let sImg5 = new slideImg("slider_image5", "this is coffee beens!", "images/coffee-6632524_1280.jpg","center", "c1");
+
+//インスタンス作成　名称、テキスト、画像URL、画像位置、文字色、クリック先のURL
+let sImg1 = new slideImg("slider_image1", "　", "images/linkedin_banner_image_2.png","left", "c1", " ");
+let sImg2 = new slideImg("slider_image2", "【特集】プレゼント企画！", "images/pancakes-5989136_1280.jpg","center", "c3","special.html");
+let sImg3 = new slideImg("slider_image3", "カフェでチーム開発?リアルイベント開催", "images/teamdev.png","right", "c1","special.html");
+let sImg4 = new slideImg("slider_image4", "【新メニュー】ドリンク？", "images/apple-pie-5479993_1280.jpg","left", "c3","special.html");
+let sImg5 = new slideImg("slider_image5", "【新メニュー】フード？", "images/coffee-6632524_1280.jpg","center", "c1","special.html");
 let sList = [sImg1,sImg2,sImg3,sImg4,sImg5];
 //各要素配置
 helperFunction.startupSlider(sList);
 
 /////////  イベント
-// スライダー押されたときにイベント
+// スライダー押されたときに発生するイベント、別ページへの移動
 const operatorSlider = document.querySelectorAll(".slider__content");
 for(let i = 0; i < operatorSlider.length; i++){
     let opElement = operatorSlider[i];
     opElement.addEventListener("click", function(){
-        alert(sList[i].explanation);
-        console.log("イベント");
+        let nextPage = document.createElement("a");
+        nextPage.href = sList[i].nextUrl;
+        nextPage.click();
     })
 }
